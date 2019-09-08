7c055d80677c20bd7c73aec48c03d88ac6b05903
INDECOMM\Devappa.Arali <Devappa.Arali@indecomm.com>
2019-09-08 13:37:02 +0530

    Added FCM functionality.

---
+++ parent/TransporterCore/src/main/java/com/transporter/notifications/TransporterPushNotifications.java
@@ -0,0 +1,45 @@
+package com.transporter.notifications;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class TransporterPushNotifications {
+	
+	@Value("${android.fcm.key}")
+	private String androidFcmKey;
+	
+	@Value("${android.fcm.url}")
+	private String androidFcmUrl;
+	
+	public String pushNotifications(String[] devicesToken) {
+		String response = null;
+		try {
+			RestTemplate restTemplate = new RestTemplate();
+		    HttpHeaders httpHeaders = new HttpHeaders();
+		    httpHeaders.set("Authorization", "key=" + androidFcmKey);
+		    httpHeaders.set("Content-Type", "application/json");
+		    JSONObject msg = new JSONObject();
+		    JSONObject json = new JSONObject();
+		
+		    msg.put("title", "Test Purpose");
+		    msg.put("body", "Dev I Arali");
+		    msg.put("notificationType", "Ganesh");
+		
+		    json.put("data", msg);
+		    json.put("registration_ids", devicesToken);
+		
+		    HttpEntity<String> httpEntity = new HttpEntity<String>(json.toString(),httpHeaders);
+		    response = restTemplate.postForObject(androidFcmUrl,httpEntity,String.class);
+		    System.out.println(response);
+		} catch (JSONException e) {
+		   e.printStackTrace();
+		}
+		return response;
+	}
+}
--- parent/TransporterCore/src/main/java/com/transporter/service/impl/TripDetailsServiceImpl.java
+++ parent/TransporterCore/src/main/java/com/transporter/service/impl/TripDetailsServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.transporter.model.TripDetails;
+import com.transporter.notifications.TransporterPushNotifications;
 import com.transporter.repo.TripDetailsRepo;
 import com.transporter.service.TripDetailsService;
 
@@ -20,11 +21,22 @@ public class TripDetailsServiceImpl implements TripDetailsService{
 	@Autowired
 	TripDetailsRepo  tripDetailsRepo;
 	
+	@Autowired
+	private TransporterPushNotifications transporterPushNotifications;
+	
 	@Override
 	public List<TripDetails> getTripHistory(int id, int tripstatus) {
 		// TODO Auto-generated method stub
 		List<TripDetails> history = tripDetailsRepo.getHistory(id,tripstatus);
 		return history;
+	}
+
+	@Override
+	public String checkVehicleAvailability(String lattitude, String longitude) {
+		
+		String devicesToken[] = {"euRR9XSIZCE:APA91bEWht_WfZjySzUCiKDTaQbNNP8smnx7snHy3jeIAFvxVVZqRmsU1ffvjuDBhFEOiXSjMW-UteYXPeECuv8Il6h4SfGWdGnj3KJbgYJLgscs-4f_N7Lxbp72umt_JYak_Q20Bh7V"};
+		String response  = transporterPushNotifications.pushNotifications(devicesToken);
+		return response;
 	}
 
 }
