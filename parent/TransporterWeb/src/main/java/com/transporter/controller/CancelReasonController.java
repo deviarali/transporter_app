0e708581f82dd516129b569288ebdc5b28b6cd74
basavaraj_navi <bnavi1992@gmail.com>
2021-03-06 00:35:25 +0530

    added the cancel reasons

--- parent/TransporterWeb/src/main/java/com/transporter/controller/CancelReasonController.java
+++ parent/TransporterWeb/src/main/java/com/transporter/controller/CancelReasonController.java
@@ -2,9 +2,12 @@ package com.transporter.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,4 +41,38 @@ public class CancelReasonController {
 		return response;
 	}
 
+	@RequestMapping(value = "cancel/saveCancelReasons", method = RequestMethod.POST)
+	public CommonResponse addCancelReasons(@Valid @RequestBody CancelReasons reason) {
+		LOG.debug("add cancel reason called");
+		CommonResponse response = null;
+		if(!reason.getCancelReason().isEmpty()){
+			String status  = cancelReasonService.addCancelReaseon(reason);
+			if (status != null && !status.isEmpty()) {
+				response = RestUtils.wrapObjectForSuccess(status);
+			} else {
+				response = RestUtils.wrapObjectForFailure("Unanble to add reason", "error",
+						WebConstants.WEB_RESPONSE_ERROR);
+			}
+		}
+		return response;
+	}
+	
+	@RequestMapping(value = "cancel/deleteCancelReasons", method = RequestMethod.DELETE)
+	public CommonResponse deleteCancelReasons(@Valid @RequestBody CancelReasons reason) {
+		LOG.debug("delete cancel reason called");
+		CommonResponse response = null;
+		if(reason.getId() != 0){
+			int res  = cancelReasonService.deleteCancelReason(reason);
+			if (res != 0) {
+				response = RestUtils.wrapObjectForSuccess(WebConstants.SUCCESS);
+			} else {
+				response = RestUtils.wrapObjectForFailure("Unanble to delete reason", "error",
+						WebConstants.WEB_RESPONSE_ERROR);
+
+			}
+		}
+		return response;
+	}
+
+	
 }
