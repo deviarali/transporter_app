bfd26a8edee799124cd0f14233a5749b22185c1c
OPENTEXT\darali <ganid005@gmail.com>
2021-03-06 16:15:07 +0530

    Driver Controller added CrossOrigin annotation.

--- parent/TransporterWeb/src/main/java/com/transporter/controller/DriverController.java
+++ parent/TransporterWeb/src/main/java/com/transporter/controller/DriverController.java
@@ -6,6 +6,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,6 +33,9 @@ import com.transporter.vo.DriverDetailsVo;
  *
  */
 
+
+
+@CrossOrigin(origins = "*")
 @RestController
 public class DriverController {
 
